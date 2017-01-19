c1aa45382253f18cb249b3eda1539527c78c9c8e
Daiderd Jordan <daiderd@gmail.com>
2017-01-19 22:17:38 +0100

    add basic packages to release.nix

--- release.nix
+++ release.nix
@@ -15,7 +15,16 @@ let
 
   defaultPackages = {
     inherit pkgs;
-    inherit (pkgs) stdenv;
+    inherit (pkgs) stdenv
+      autoconf automake bison bzip2 clang cmake coreutils cpio ed findutils flex gawk gettext gmp
+      gnugrep gnum4 gnumake gnused groff gzip help2man libcxx libcxxabi libedit libffi libtool
+      libxml2 llvm ncurses patch pcre perl pkgconfig python unzip xz zlib;
+    darwin = pkgs.recurseIntoAttrs {
+      inherit (pkgs.darwin)
+        CF CarbonHeaders CommonCrypto Csu IOKit Libinfo Libm Libnotify Libsystem adv_cmds
+        architecture bootstrap_cmds bsdmake cctools configd copyfile dyld eap8021x launchd
+        libclosure libdispatch libiconv libpthread libresolv libutil objc4 ppp removefile xnu;
+    };
   };
 
   extraPackages = filterPkgs packageAttrs;
